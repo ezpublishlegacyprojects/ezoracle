9079bcd77f2f0508fffa525b61df10a8f03e0c32
ls <info@example.com>
2010-10-13 09:09:04 +0000

    - Removed 2.1.0 release
    git-svn-id: http://svn.projects.ez.no/ezoracle@524 436571c7-bdc5-44d1-b1ef-e55a79f94445

--- releases/2.1.0/extension/ezoracle/update/database/ezoracle/2.0/dbupdate-2.0.3-2.0.4.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE ezenumvalue MODIFY ( enumelement NULL );
-ALTER TABLE ezenumvalue MODIFY ( enumvalue NULL );
-ALTER TABLE ezenumobjectvalue MODIFY ( enumvalue NULL );
-ALTER TABLE ezcontentclass_name MODIFY ( name NULL );
--- releases/2.1.0/extension/ezoracle/update/database/ezpublish/4.3/dbupdate-4.3.0-to-4.3.1.sql
+++
@@ -1,4 +0,0 @@
-UPDATE ezsite_data SET value='4.3.1' WHERE name='ezpublish-version';
-UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
-
-UPDATE ezcontentclass_attribute SET can_translate=0 WHERE data_type_string='ezuser';
